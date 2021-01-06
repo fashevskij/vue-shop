4af4799d5a53119463f3f1d4b3ced00b10f6e5fd
fashevskij <68337800+fashevskij@users.noreply.github.com>
2021-01-06 11:37:32 +0200

    slot vue test

---
+++ one.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="hello">
+		{{msg}}
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'hello',
+	data() {
+		return {
+			msg: 'welcom vue.js app'
+		}
+	}
+}
+</script>
+
+<style lang="css" scoped>
+	
+</style>
